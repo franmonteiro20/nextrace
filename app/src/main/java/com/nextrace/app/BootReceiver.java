9e8b2940ef8ec26131ff496a060b410a765689ca
franmonteiro20 <85819874+franmonteiro20@users.noreply.github.com>
2026-09-21 17:36:39 -0300

    NextRace v1.5: app/src/main/java/com/nextrace/app/BootReceiver.java

---
+++ app/src/main/java/com/nextrace/app/BootReceiver.java
@@ -0,0 +1,12 @@
+package com.nextrace.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationScheduler.rescheduleSaved(context);
+    }
+}
