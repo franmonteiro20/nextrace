1e5d5ce6a038e55bd4fced8a42c36a59ceb3c2af
franmonteiro20 <85819874+franmonteiro20@users.noreply.github.com>
2026-09-21 07:57:10 -0300

    Publica NextRace v1.0.0

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='nextrace-v1.0.0';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x));return r;}).catch(()=>caches.match('./index.html'))));});
