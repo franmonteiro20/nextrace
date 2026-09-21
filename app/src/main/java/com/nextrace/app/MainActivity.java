895738a91aa7f2e5f9a1d200cfa3dcd67ac83047
franmonteiro20 <85819874+franmonteiro20@users.noreply.github.com>
2026-09-21 09:47:58 -0300

    Android: carrega NextRace v1.2.0

--- app/src/main/java/com/nextrace/app/MainActivity.java
+++ app/src/main/java/com/nextrace/app/MainActivity.java
@@ -15,7 +15,7 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     private WebView webView;
-    private static final String APP_URL = "https://franmonteiro20.github.io/nextrace/?v=1.1.2";
+    private static final String APP_URL = "https://franmonteiro20.github.io/nextrace/?v=1.2.0";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
