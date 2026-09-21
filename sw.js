8c85468bce3446dc53e1a4559e8414358af6fd4b
franmonteiro20 <85819874+franmonteiro20@users.noreply.github.com>
2026-09-21 09:48:01 -0300

    NextRace v1.2.0: atualiza cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nextrace-v1.1.2';
+const CACHE='nextrace-v1.2.0';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
